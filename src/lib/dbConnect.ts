16db53df4263a03a3eabdce00d9d1a7f71b4a8d6
Eshu54321 <eshuedit@gmail.com>
2026-03-24 15:37:40 +0530

    Fix: Make dbConnect build-friendly

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -9,10 +9,6 @@ declare global {
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
@@ -20,6 +16,9 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  if (!MONGODB_URI) {
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  }
   if (cached.conn) {
     return cached.conn;
   }
